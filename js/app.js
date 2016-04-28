a64a917c4963eb21d96c8cf5fce1eb50216aea06
Marco Vito Moscaritolo <mavimo@gmail.com>
2016-04-28 09:58:44 +0200

    step 2

--- js/app.js
+++ js/app.js
@@ -5,11 +5,28 @@ var app = angular.module('codemasterApp', [
     'codemaster.controllers'
   ])
 
+app.constant('codemasterConfig', {
+    'apiKey' : 'INSERT_CONFIG_HERE',
+    'baseUrl' : 'https://api.themoviedb.org/3',
+
+    getWebServiceUrl: function (info) {
+      return this.baseUrl + info + '?language=IT&api_key=' + this.apiKey;
+    }
+  })
+
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
       when('/', {
         controller: 'HomeCtrl',
         templateUrl: '/views/home.html'
+      }).
+      when('/generi', {
+        controller: 'GeneriCtrl',
+        templateUrl: '/views/generi.html'
+      }).
+      when('/generi/:id', {
+        controller: 'GenereCtrl',
+        templateUrl: '/views/genere.html'
       }).
       when('/info', {
         controller: 'InfoCtrl',
--- js/controller.js
+++ js/controller.js
@@ -2,6 +2,36 @@
 
 /* Directives */
 angular.module('codemaster.controllers', [])
+  .controller('GeneriCtrl', ['$scope', '$http', 'codemasterConfig', function ($scope, $http, config) {
+    $scope.titolo = "Elenco generi";
+    $scope.genres = [];
+    $scope.error = false;
+
+    $http({
+      method: 'GET',
+      url: config.getWebServiceUrl('/genre/movie/list')
+    }).then(function success(response) {
+        $scope.genres = response.data.genres;
+      }, function error(response) {
+        $scope.error = true;
+      });
+  }])
+
+  .controller('GenereCtrl', ['$scope', '$routeParams', '$http', 'codemasterConfig', function ($scope, $routeParams, $http, config) {
+    $scope.titolo = '';
+    $scope.movies = [];
+    $scope.error = false;
+
+    $http({
+      method: 'GET',
+      url: config.getWebServiceUrl('/genre/' + $routeParams.id + '/movies')
+    }).then(function success(response) {
+        $scope.movies = response.data.results;
+      }, function error(response) {
+        $scope.error = true;
+      });
+  }])
+
   .controller('HomeCtrl', ['$scope', function ($scope) {
     $scope.titolo = "HOME";
     $scope.messaggio = "Benvenuto nella nostra app!";
